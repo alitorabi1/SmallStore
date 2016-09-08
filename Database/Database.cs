6088a5a1c48b8b7fd3dda0a1bd6b5d1475926033
Behnaz <behnazakbaria@yahoo.com>
2016-09-08 12:01:16 -0400

    implement  some  DB methods * LoginAdminToStore,LoginCashierToStore *addEmployee,addCustomer

--- Database/Database.cs
+++ Database/Database.cs
@@ -21,12 +21,12 @@ namespace SmallStore
             SqlCommand cmd = new SqlCommand("SELECT * FROM Employee WHERE UserName=@Username and Password=@Password and IsManager=1", conn);
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Parameters.AddWithValue("@Username", e.UserName);
-            cmd.Parameters.AddWithValue("@Password", e.UserName);
+            cmd.Parameters.AddWithValue("@Password", e.Password);
 
 
             using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                int rowCount=0;
+                int rowCount = 0;
 
                 if (reader.HasRows)
                 {
@@ -35,17 +35,18 @@ namespace SmallStore
                     {
                         rowCount++;
                     }
-                    
+
                 }
                 if (rowCount == 1) return true;
                 return false;
             }
         }
-        public bool LoginCashierToStore(Employee e) {
+        public bool LoginCashierToStore(Employee e)
+        {
             SqlCommand cmd = new SqlCommand("SELECT * FROM Employee WHERE UserName=@Username and Password=@Password and IsManager=0", conn);
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Parameters.AddWithValue("@Username", e.UserName);
-            cmd.Parameters.AddWithValue("@Password", e.UserName);
+            cmd.Parameters.AddWithValue("@Password", e.Password);
 
 
             using (SqlDataReader reader = cmd.ExecuteReader())
@@ -66,6 +67,54 @@ namespace SmallStore
             }
         }
 
+        public void addEmployee(Employee e)
+        {
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO Employee (FirstName, LastName,Phone,CellPhone,Address,PostalCode,BirthDate,JobTitle,HireDate,FireDate,Salary,IsPermenant,IsManager,UserName,Password) VALUES (@FirstName, @LastName,@Phone,@CellPhone,@Address,@PostalCode,@BirthDate,@JobTitle,@HireDate,@FireDate,@Salary,@IsPermenant,@IsManager,@UserName,@Password)"))
+            {
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@FirstName", e.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", e.LastName);
+                cmd.Parameters.AddWithValue("@Phone", e.Phone);
+                cmd.Parameters.AddWithValue("@CellPhone", e.CellPhone);|
+                 cmd.Parameters.AddWithValue("@Address", e.Address);
+                cmd.Parameters.AddWithValue("@PostalCode", e.PostalCode);
+                cmd.Parameters.AddWithValue("@BirthDate", e.BirthDate);
+                cmd.Parameters.AddWithValue("@JobTitle", e.JobTitle);
+                cmd.Parameters.AddWithValue("@HireDate", e.HireDate);
+                cmd.Parameters.AddWithValue("@FireDate", e.FireDate);
+                cmd.Parameters.AddWithValue("@Salary", e.Salary);
+                cmd.Parameters.AddWithValue("@IsPermenant", e.IsPermenant);
+                cmd.Parameters.AddWithValue("@IsManager", e.IsManager);
+                cmd.Parameters.AddWithValue("@UserName", e.UserName);
+                cmd.Parameters.AddWithValue("@Password", e.Password);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+        public void addCustomer(Customer c)
+        {
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO Customer (FirstName, LastName,Phone,CellPhone,Address,PostalCode,DiscountPercentage,Reg``isterDate,ModifyDate) VALUES (@FirstName, @LastName,@Phone,@CellPhone,@Address,@PostalCode,@DiscountPercentage,@RegisterDate,@ModifyDate)"))
+            {
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@Phone", c.Phone);
+                cmd.Parameters.AddWithValue("@CellPhone", c.CellPhone);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@PostalCode", c.PostalCode);
+                cmd.Parameters.AddWithValue("@DiscountPercentage", c.DiscountPercentage);
+                cmd.Parameters.AddWithValue("@RegisterDate", c.RegisterDate);
+                cmd.Parameters.AddWithValue("@ModifyDate", c.ModifyDate);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
 
 
     }
