f8e09da8a21b39a0375e92a3cfaeb0acb433937d
Ali Torabi <alitorabi1@gmail.com>
2016-09-08 21:23:54 -0700

    --- Ali -- Opening forms, Passing variables between forms, Using ribbons -- Ali --

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -25,5 +25,48 @@ namespace SmallStore
             InitializeComponent();
             tbUsername.Focus();
         }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if ( tbUsername.Text.Length == 0 || pbPassword.Password.Length == 0 )
+            {
+                lblErrorMessage.Content = "Please enter username or password.";
+                return;
+            }
+
+            if ( isAuthenticated() == "ADMINISTRATOR")
+            {
+                Admin dialog = new Admin();
+                dialog.lblWelcome.Content = "Welcome: " + tbUsername.Text;
+                dialog.ShowDialog();
+                this.Close();
+            }
+            else if (isAuthenticated() == "CASHIER" )
+            {
+                Cashier dialog = new Cashier();
+                dialog.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                lblErrorMessage.Content = "Invalid Username or Password. Please Try again";
+                return;
+            }
+        }
+
+        private string isAuthenticated()
+        {
+            // TODO: this method checks the role of user and authenticate him
+
+            //            throw new NotImplementedException();
+            //            return "CASHIER";
+            return "ADMINISTRATOR";
+            //return "";
+        }
+
+        private void tbUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            lblErrorMessage.Content = "";
+        }
     }
 }
